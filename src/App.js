ee1b29b427959f6b4202dafd97a47fa5f30ebbb7
carlsachs <sachscarl@gmail.com>
2020-02-11 11:42:49 -0500

    was tired and forgot to commit, committing and pushing now

--- src/App.js
+++ src/App.js
@@ -1,16 +1,83 @@
 import React from 'react';
+import TodoList from './components/TodoComponents/TodoList';
+import TodoForm from './components/TodoComponents/TodoForm';
+import styled from "styled-components";
 
+const Header = styled.div`
+    background-color: blue;
+    display: flex;
+    justify-content: center;
+    flex-flow: column nowrap;
+`;
+
+
+const items = [
+    {
+        task: 'Do Laundry',
+        id: Date.now(),
+        completed: false
+    },
+    {
+        task: 'Wash Dishes',
+        id: Date.now(),
+        completed: false 
+    }
+];
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
-  render() {
-    return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
-      </div>
-    );
-  }
+    constructor(){
+        super();
+        this.state = {
+            itemsList: items
+        };
+    }
+    
+    toggleItem = clickedId => {
+        const newItemsList = this.state.itemsList.map(item => {
+            if (item.id === clickedId) {
+                return {
+                    ...item,
+                    completed: !item.completed
+                };
+            } else {
+                return item;
+            }
+        });
+        this.setState({
+            itemsList: newItemsList
+        });
+    };
+    addNewItem = itemText => {
+        const newItem = {
+            name: itemText,
+            id: Date.now(),
+            purchased: false
+        };
+        this.setState({
+            itemsList: [...this.state.itemsList, newItem]
+        });
+    };
+    itemDelete = () => {
+      const resetList = this.state.itemsList.filter(items => !items.completed)
+      this.setState({
+        itemsList: resetList
+      })
+    }
+    render() {
+        return (
+            <div className="App">
+                <Header>
+                  <h1>Shopping List</h1>
+                  <TodoForm 
+                    addNewItem={this.addNewItem} 
+                    itemDelete={this.itemDelete}
+                  />
+                </Header>
+                  <TodoList 
+                    items={this.state.itemsList} 
+                    toggleItem={this.toggleItem}
+                  />
+            </div>
+        );
+    }
 }
-
 export default App;
